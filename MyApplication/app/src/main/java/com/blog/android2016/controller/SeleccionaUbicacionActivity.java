287714deea364f857df4dcee7cfb176510e8d36e
beta3000 <atlas.aresco@gmail.com>
2016-02-15 02:42:59 -0500

    Splash agregado
    Splash Full Screen

---
+++ MyApplication/app/src/main/java/com/blog/android2016/controller/SeleccionaUbicacionActivity.java
@@ -0,0 +1,16 @@
+package com.blog.android2016.controller;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.blog.android2016.R;
+
+public class SeleccionaUbicacionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecciona_ubicacion);
+    }
+
+}
